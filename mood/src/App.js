c5c1dfd28f4a0db6c5ab2b81802344cce9accaaf
Tiffany Drouet <tiffanydrouet96@gmail.com>
2019-05-28 22:59:11 +0200

    add folder image and test to call the tab params on back

--- mood/src/App.js
+++ mood/src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-        <div id="background"></div>
-        <div className="mood_main">
-            <div className="mood_col-main">
-                <div className="mood_container">
-                  <Header />
-                </div>
-            </div> 
-        </div>  
-      </div>     
-  );
+class App extends Component {
+  state = {
+    params : [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/params')
+    .then(res => res.json())
+    .then(params => this.setState( { params } ))
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <div id="background"></div>
+          <div className="mood_main">
+              <div className="mood_col-main">
+                  <div className="mood_container">
+                    <Header />
+                    <div id="params">
+                      {this.state.params.map((x) => <div className={`choice_element ${x.element}`}>
+                      <img src={x.image} alt="test"></img>
+                      </div>)}
+                    </div>
+                  </div>
+              </div> 
+          </div>  
+        </div>     
+    );
+  }  
 }
 
 export default App;
