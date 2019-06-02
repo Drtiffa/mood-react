146897dd4167be7d1ec4b02b8c852c41fdc3295b
Tiffany Drouet <tiffanydrouet96@gmail.com>
2019-06-02 23:00:19 +0200

    page creation avatar with info back climb in front

--- mood/src/App.js
+++ mood/src/App.js
@@ -1,38 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.scss';
 import Header from './components/Header';
-
-class App extends Component {
-  state = {
-    params : [],
-  }
-
-  componentDidMount() {
-    fetch('http://localhost:3001/params')
-    .then(res => res.json())
-    .then(params => this.setState( { params } ))
-    .catch(err => console.log(err))
-  }
-
-  render() {
-    return (
-      <div className="App">
-          <div id="background"></div>
-          <div className="mood_main">
-              <div className="mood_col-main">
-                  <div className="mood_container">
-                    <Header />
-                    <div id="params">
-                      {this.state.params.map((x) => <div className={`choice_element ${x.element}`}>
-                      <img src={x.image} alt="test"></img>
-                      </div>)}
-                    </div>
-                  </div>
-              </div> 
-          </div>  
-        </div>     
-    );
-  }  
-}
+import CreateAvatar from './components/CreateAvatar';
+	
+function App() {
+  return (
+    <div className="App">
+        <div id="background"></div>
+        <div className="mood_main">
+            <div className="mood_col-main">
+                <div className="mood_container">
+                  <Header />
+                  <CreateAvatar />
+                </div>
+            </div> 
+        </div>  
+      </div>     
+  ); 
+}  
 
 export default App;
---
+++ mood/src/components/CreateAvatar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class CreateAvatar extends Component {
+  state = {
+    params : [],
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/params')
+    .then(res => res.json())
+    .then(params => this.setState( { params } ))
+    .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+        <div id="params">
+            {this.state.params.map((x) => <div className={`choice_element ${x.element}`}>
+            <img src={x.image} alt="test"></img>
+            </div>)}
+        </div>      
+    );
+  }  
+}
+
+export default CreateAvatar;
