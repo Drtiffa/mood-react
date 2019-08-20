0074a1938c2ace7ddc0fddea161ab4dd906b22c7
Tiffany Drouet <tiffanydrouet96@gmail.com>
2019-08-20 15:16:23 +0200

    Updates

---
+++ precache-manifest.a62904de32533cbb21423978def3d0d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbd666369bae93b36654d2d464e1a24",
+    "url": "/mood-react/mood/index.html"
+  },
+  {
+    "revision": "a58fb494835f03af4825",
+    "url": "/mood-react/mood/static/css/main.1eac764c.chunk.css"
+  },
+  {
+    "revision": "323eac48082df29b8513",
+    "url": "/mood-react/mood/static/js/2.ad0252cd.chunk.js"
+  },
+  {
+    "revision": "a58fb494835f03af4825",
+    "url": "/mood-react/mood/static/js/main.d0a81ce4.chunk.js"
+  },
+  {
+    "revision": "144c84d330687e4ca045",
+    "url": "/mood-react/mood/static/js/runtime~main.578b5aff.js"
+  },
+  {
+    "revision": "d4bedf9c7394d7a79f67c1d5febfa9b2",
+    "url": "/mood-react/mood/static/media/DKBuntaro.d4bedf9c.ttf"
+  },
+  {
+    "revision": "fa9dd4966e1a33a15407b49b8f977391",
+    "url": "/mood-react/mood/static/media/HILVCC+Boing-Bold.fa9dd496.ttf"
+  },
+  {
+    "revision": "a3c61a64a1fc89b4c1535a08c5d5be0a",
+    "url": "/mood-react/mood/static/media/HILVCC+Boing-Light.a3c61a64.ttf"
+  }
+]);
